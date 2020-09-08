12673bd61d828d77bbfecf9c28f1b3ee1c71528f
Ruslan Pryimak <pryimak.rv@gmail.com>
2020-09-08 22:48:27 +0200

    Added BaseClass

---
+++ src/test/java/com/epam/ta/tests/BaseClass.java
@@ -0,0 +1,10 @@
+package com.epam.ta.tests;
+
+public class BaseClass {
+
+    private static String BASE_URL = "http://jsonplaceholder.typicode.com";
+
+    public static String getBaseUrl() {
+        return BASE_URL;
+    }
+}
