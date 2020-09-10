7902e958fe4bb6b31fc7fc1b72dba80ccfafc058
Ruslan Pryimak <pryimak.rv@gmail.com>
2020-09-10 17:01:50 +0200

    Added new methods to PostSteps

--- src/test/java/com/epam/ta/steps/PostSteps.java
+++ src/test/java/com/epam/ta/steps/PostSteps.java
@@ -31,4 +31,30 @@ public class PostSteps {
         }
     }
 
+    public static void verifyPostDeleted(Post actualPost) {
+        try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+            softly.assertThat(actualPost.getId())
+                    .isNull();
+            softly.assertThat(actualPost.getUserId())
+                    .isNull();
+            softly.assertThat(actualPost.getTitle())
+                    .isNull();
+            softly.assertThat(actualPost.getBody())
+                    .isNull();
+        }
+    }
+
+    public static void verifyPostTitle(Post actualPost, int id, String testTitle) {
+        try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
+            softly.assertThat(actualPost.getId())
+                    .isEqualTo(id);
+            softly.assertThat(actualPost.getUserId())
+                    .isNotNull();
+            softly.assertThat(actualPost.getTitle())
+                    .isEqualTo(testTitle);
+            softly.assertThat(actualPost.getBody())
+                    .isNotNull();
+        }
+
+    }
 }
