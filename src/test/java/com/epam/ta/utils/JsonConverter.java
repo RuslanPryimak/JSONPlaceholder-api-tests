5a6ab53f8c83da4684fe103bb46cee65a216fd46
Ruslan Pryimak <pryimak.rv@gmail.com>
2020-09-14 22:26:22 +0200

    Added JsonConverter

---
+++ src/test/java/com/epam/ta/utils/JsonConverter.java
@@ -0,0 +1,15 @@
+package com.epam.ta.utils;
+
+import com.google.gson.Gson;
+import io.restassured.response.Response;
+
+import java.lang.reflect.Type;
+
+public class JsonConverter {
+
+    private static Gson GSON = new Gson();
+
+    public static <T> T convertToJson(Response response, Class<T> clazz) {
+        return GSON.fromJson(response.jsonPath().prettify(), (Type) clazz);
+    }
+}
